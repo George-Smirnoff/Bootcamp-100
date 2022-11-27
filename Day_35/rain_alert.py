e5c5c1da88d1a061d36722f2f9654a9f8f3f3e3f
Georgij Smirnov <georgijsmirnov@Georgijs-MacBook-Pro.local>
2022-11-27 17:15:50 +0100

    Day 35: Create simple alerting notification with twilio

---
+++ Day_35/rain_alert.py
@@ -0,0 +1,42 @@
+import requests
+import os
+from twilio.rest import Client
+
+def main():
+    url = "https://api.openweathermap.org/data/2.5/forecast"
+
+    myLatitude = 48.8588548
+    myLongitude = 2.347035
+    send_sms = False
+    api_key = os.getenv('API_KEY')
+
+    parameters = {
+        'lat': myLatitude,
+        'lon': myLongitude,
+        'appid': api_key
+    }
+
+    r = requests.get(url=url, params=parameters)
+
+    weatherData = r.json()
+
+    weatherList = weatherData["list"]
+    for item in weatherList[:4]:
+        if item["weather"][0]["id"] < 600:
+            print(f'RAINY! at {item["dt_txt"]}')
+            send_sms = True
+
+    if send_sms:
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token)
+
+        client.messages.create(
+            body='Hello,\nWe are expecting a rain this day. Take your umbrella!',
+            from_='SENDER',
+            to='RECEIVER'
+        )
+        print('Message sent.')
+
+if __name__ == '__main__':
+    main()
